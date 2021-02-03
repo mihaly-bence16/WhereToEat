e01c6151a01102b0c300ac9e54713c0f38758394
unknown <mihaly.bence16@gmail.com>
2021-02-03 18:31:46 +0200

    Initial commit

---
+++ app/src/main/java/com/example/wheretoeat/ui/list/ListFragment.kt
@@ -0,0 +1,8 @@
+package com.example.wheretoeat.ui.list
+
+import androidx.fragment.app.Fragment
+import com.example.wheretoeat.R
+
+class ListFragment : Fragment(R.layout.fragment_restaurant_list){
+
+}
