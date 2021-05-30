831a1a7750a3b1fa91b459b60b72e8a5e818d8b0
mihaly-bence16 <73333072+mihaly-bence16@users.noreply.github.com>
2021-05-30 13:06:47 +0300

    removed accidentally added file

--- app/src/main/java/com/example/wheretoeat/data/Favorite/FavoriteDao.kt
+++ app/src/main/java/com/example/wheretoeat/data/Favorite/FavoriteDao.kt
@@ -12,9 +12,12 @@ interface FavoriteDao {
     @Query("SELECT * FROM favorite_table ORDER BY id ASC")
     fun readAllFavorite() : LiveData<List<Favorite>>
 
-    @Delete
-    suspend fun deleteFavorite(favorite: Favorite)
+    @Query("DELETE FROM favorite_table WHERE restaurant_id=:restaurant_id AND user_id=:user_id")
+    suspend fun deleteFavorite(restaurant_id: String,user_id:Int)
 
     @Query("SELECT COUNT(*) FROM favorite_table")
     fun getCount(): LiveData<Int>
+
+    @Query("SELECT * FROM favorite_table WHERE user_id=:id")
+    fun readAllFavoritesByCurrentId(id : Int) : List<Favorite>
 }
